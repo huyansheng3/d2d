0b472de7751d83e0cac60602e762609ea6d65b2a
hys <huyansheng@fishtrip.cn>
2018-04-21 13:48:19 +0800

    fix: 强制去掉sw 注册

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+unregister();
 // registerServiceWorker();
