ff39ce8cd45112592a568ee3c96978b4af4267f2
hys <huyansheng@fishtrip.cn>
2018-05-06 09:40:38 +0800

    fix: 修复域名

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -10,12 +10,12 @@ export const SERVER_HOST =
     : 'http://localhost:3030';
 
 // 区块浏览器的服务器
-export const BLOCK_SERVER_HOST = '//explorer.everchain.net';
+export const BLOCK_SERVER_HOST = 'http://explorer.everchain.net';
 
 // 易工程的服务器
-export const EASY_PROJECT_HOST = '//110.80.142.87:10009';
+export const EASY_PROJECT_HOST = 'http://110.80.142.87:10009';
 
-export const ABS = '//47.92.26.19:8082';
-export const ABS_HOST = '//47.92.26.19:8093';
+export const ABS = 'http://47.92.26.19:8082';
+export const ABS_HOST = 'http://47.92.26.19:8093';
 
 export const SERVER_TIMEOUT = 10000;
